f8e48b76745c47ad0330310ac21ded6b0ae13b86
CFournier59 <clementfournier.c@gmail.com>
2026-01-20 14:53:03 +0100

    création des routes  sans authentification : - getAllBooks -getBestBooks -getThisBook

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,14 +1,28 @@
 const Book = require('../models/Book');
 
+exports.getAllBooks = (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));}
+
+exports.getBestBooks = (req, res, next) => {
+    Book.find().sort({ averageRating: -1 }).limit(3)
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+}
+
+exports.getThisBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+}
+
+
+
 exports.createBook = (req, res, next) => {}
 
 exports.modifyBook = (req, res, next) => {}
 
 exports.deleteBook = (req, res, next) => {}
 
-exports.getAllBooks = (req, res, next) => {
-    Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));}
 
-exports.getThisBook = (req, res, next) => {}
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
+router.get('/bestrating', bookCtrl.getBestBooks);
+router.get('/:id', bookCtrl.getThisBook);
+// router.post('/', bookCtrl.createBook);
+// router.put('/:id', bookCtrl.modifyBook);
+// router.delete('/:id', bookCtrl.deleteBook);
 
 module.exports = router;
